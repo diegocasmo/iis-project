1020b919380c3c5f55bae4d6b0be12ac23d9a638
Tristan <thrawst88@gmail.com>
2018-05-04 14:06:27 +0200

    initial lm3 parser

---
+++ code/lm3-analysis/constants.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3.4
+# -*- coding: utf-8 -*-
+
+def get_all_emotions():
+  '''
+  List of all possible emotions
+  '''
+  return [
+    'ANGER',
+    'DISGUST',
+    'FEAR',
+    'HAPPY',
+    'SADNESS',
+    'SURPRISE'
+  ]
+
+def get_all_landmarks():
+  '''
+  An ordered list of all possible landmarks
+  '''
+  return [
+    'Outer left eyebrow',
+    'Middle left eyebrow',
+    'Inner left eyebrow',
+    'Inner right eyebrow',
+    'Middle right eyebrow',
+    'Outer right eyebrow',
+    'Left temple',
+    'Outer left eye corner',
+    'Inner left eye corner',
+    'Inner right eye corner',
+    'Outer right eye corner',
+    'Right temple',
+    'Nose saddle left',
+    'Nose saddle right',
+    'Left nose peak',
+    'Nose tip',
+    'Right nose peak',
+    'Left mouth corner',
+    'Upper lip outer middle',
+    'Right mouth corner',
+    'Upper lip inner middle',
+    'Lower lip inner middle',
+    'Lower lip outer middle',
+    'Chin middle',
+    'Left ear lobe',
+    'Right ear lobe'
+  ]
